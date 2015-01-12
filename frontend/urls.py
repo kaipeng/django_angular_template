0e20f54db7188d32ff7e7fd8476dc65af662cfce
Kai Peng <kpeng234@gmail.com>
2015-01-12 11:19:16 -0600

    Adding templates

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,10 +2,13 @@
 from django.conf.urls import patterns, url
 from django.conf import settings
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = patterns("frontend.views",
-                       ("^$", TemplateView.as_view(template_name="site/index.html"))
+                       url(r'^$', RedirectView.as_view(url='site/', permanent=True)),
+                       ("^site/$", TemplateView.as_view(template_name="frontend.html"))
                        )
 urlpatterns += patterns('django.contrib.staticfiles.views',
-                        url(r'^site/(?P<path>.*)$', 'serve'),
+                        url(r'^(?P<path>.*)$', 'serve'),
                         )
